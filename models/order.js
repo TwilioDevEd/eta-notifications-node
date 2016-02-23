de2b97ad2c69ee14efaddff0c57d1bc868fce549
nebtrx <neb.trx@gmail.com>
2016-02-23 10:53:46 -0500

    Add order status change features

--- models/order.js
+++ models/order.js
@@ -9,5 +9,32 @@ var OrderSchema = new mongoose.Schema({
   notificationStatus : { type: String, default: 'None' },
 });
 
+OrderSchema.methods.sendSmsNotification = function (message, statusCallback , callback) {
+
+  var client = new twilio.RestClient(cfg.twilioAccountSid, cfg.twilioAuthToken);
+
+  var options = {
+      to:  this.customerPhoneNumber,
+      from: cfg.twilioPhoneNumber,
+      body: message,
+      statusCallback: statusCallback
+  };
+
+  client.sendMessage(options, function(err, response) {
+      if (err) {
+          console.error(err);
+      } else {
+          var masked = this.customerPhoneNumber.substr(0,
+            customerPhoneNumber.length - 5);
+          masked += '*****';
+          console.log('Message sent to ' + masked);
+      }
+  });
+
+  if (callback) {
+    callback.call(this);
+  }
+};
+
 var Order = mongoose.model('order', OrderSchema);
 module.exports = Order;
--- routes/orders.js
+++ routes/orders.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Order = require('../models/order');
 
-/* GET users listing. */
+// GET: /orders
 router.get('/', function(req, res, next) {
   Order.find()
     .then(function (orders) {
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
     });
 });
 
-
+// GET: /orders/4
 router.get('/:id', function(req, res, next) {
   var id = req.params.id;
   Order.findOne({_id: id })
@@ -18,5 +18,43 @@ router.get('/:id', function(req, res, next) {
       res.render('orders/show', { order });
     });
 });
+
+// POST: /orders/4/pickup
+router.post('/:orderId/pickup', function(req, res, next) {
+
+  var id = req.params.orderId;
+
+  Order.findOne({_id: id })
+    .then(function (order) {
+      order.status = 'Shipped';
+      order.notificationStatus = 'Queued';
+      order.save();
+
+      order.sendSmsNotification('Your clothes will be sent and will be delivered in 20 minutes', getCallbackUri(req) );
+      res.redirect(`/orders/${id}`);
+    });
+});
+
+// POST: /orders/4/deliver
+router.post('/:orderId/deliver', function(req, res, next) {
+
+  var id = req.params.orderId;
+  var host = req.headers.host;
+
+  Order.findOne({_id: id })
+    .then(function (order) {
+      order.status = 'Delivered';
+      order.notificationStatus = 'Queued';
+      order.save();
+
+      order.sendSmsNotification('Your clothes have been delivered', getCallbackUri(req) );
+      res.redirect(`/orders/${id}`);
+    });
+});
+
+var getCallbackUri = function(req){
+  var host = req.headers.host;
+  return `http://${host}/status`
+}
 
 module.exports = router;
