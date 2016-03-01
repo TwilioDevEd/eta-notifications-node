a460491bca29fc3a4426b340edca775e170c4d6b
nebtrx <neb.trx@gmail.com>
2016-03-01 11:24:14 -0500

    Fix seeds

--- seed/orders.js
+++ seed/orders.js
@@ -1,7 +1,7 @@
 db.dropDatabase();
 
 var orders = [
-  { customerName: 'Vincent Vega',   customerPhoneNumber: '+593999760268',  status : 'Ready', notificationStatus : 'None' },
+  { customerName: 'Vincent Vega',   customerPhoneNumber: '+17654532001',  status : 'Ready', notificationStatus : 'None' },
   { customerName: 'Mia Wallace',   customerPhoneNumber: '+17654532002' , status : 'Ready', notificationStatus : 'None'  },
 ];
 
