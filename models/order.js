fc869e2c956da1fab5bc7ac1a8ba300136673ce6
juancarlospaco <juancarlospaco@gmail.com>
2016-11-18 17:15:51 -0300

    Fix a Typo.

--- models/order.js
+++ models/order.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-var cfg = require('../config');
+var config = require('../config');
 var twilio = require('twilio');
 
 
@@ -16,7 +16,7 @@ OrderSchema.methods.sendSmsNotification = function(message, statusCallback, call
   var self = this;
   var options = {
     to: self.customerPhoneNumber,
-    from: cfg.twilioPhoneNumber,
+    from: config.twilioPhoneNumber,
     body: message,
     statusCallback: statusCallback,
   };
