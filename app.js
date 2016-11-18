3e0e9a37a77c19a9a9a92d460047e1f0477cd699
juancarlospaco <juancarlospaco@gmail.com>
2016-11-18 18:18:41 -0300

    Remove unwanted blank new lines.

--- app.js
+++ app.js
@@ -16,8 +16,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -72,6 +70,5 @@ app.use(function(err, req, res, next) {
     error: {},
   });
 });
-
 
 module.exports = app;
