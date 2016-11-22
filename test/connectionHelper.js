f350274b21f6b306983f669dd975d339b280a335
Agustin Camino <agustin.camino@gmail.com>
2016-11-22 10:17:20 -0500

    Update .env.example to provide usage examples
    - Rename URL to URI. The connection string uses a URI format anyway.
    - Use a literal object in config.js, it improves the readability.
    - Remove database config strings fallback values.
    - Remove unnecessary values from .travis.yml

--- test/connectionHelper.js
+++ test/connectionHelper.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 var cfg = require('../config');
 
-exports.mongoConnection = mongoose.connect(cfg.mongoUrlTest);
+exports.mongoConnection = mongoose.connect(cfg.mongoUriTest);
 mongoose.Promise = Promise;
