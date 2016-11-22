c9255a85b3aba26930652069222c0d80d9a01ebd
Agustin Camino <agustin.camino@gmail.com>
2016-11-22 10:17:20 -0500

    Update .env.example to provide usage examples
    - Rename URL to URI. The connection string uses a URI format anyway.
    - Use a literal object in config.js, it improves the readability.
    - Remove database config strings fallback values.
    - Remove unnecessary values from .travis.yml

--- config.js
+++ config.js
@@ -1,31 +1,26 @@
-require('dotenv-safe').load();
-var cfg = {};
-var defaultMongoUrl = 'mongodb://localhost/eta-notifications-node';
+// Fallback values
+var defaultMongoUrl = 'mongodb://localhost:27017/eta-notifications-node';
 var defaultMongoUrlTest = 'mongodb://localhost/eta-notifications-node-test';
 
-// HTTP Port to run our web application
-cfg.port = process.env.PORT || 3000;
+module.exports = {
+  // HTTP Port to run our web application
+  port: process.env.PORT || 3000,
 
-// A random string that will help generate secure one-time passwords and
-// HTTP sessions
-cfg.secret = process.env.APP_SECRET || 'keyboard cat';
+  // A random string that will help generate secure one-time passwords and
+  // HTTP sessions
+  secret: process.env.APP_SECRET || 'keyboard cat',
 
-// Your Twilio account SID and auth token, both found at:
-// https://www.twilio.com/user/account
-//
-// A good practice is to store these string values as system environment
-// variables, and load them from there as we are doing below. Alternately,
-// you could hard code these values here as strings.
-cfg.twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
-cfg.twilioAuthToken = process.env.TWILIO_AUTH_TOKEN;
+  // Your Twilio account SID and auth token, both found at:
+  // https://www.twilio.com/user/account
+  twilioAccountSid: process.env.TWILIO_ACCOUNT_SID,
+  twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
 
-// A Twilio number you control - choose one from:
-// Specify in E.164 format, e.g. "+16519998877"
-cfg.twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
+  // A Twilio number you control - choose one from:
+  // Specify in E.164 format, e.g. "+16519998877"
+  twilioPhoneNumber: process.env.TWILIO_PHONE_NUMBER,
 
-// MongoDB connection string - MONGO_URL is for local dev,
-cfg.mongoUrl = process.env.MONGO_URL || defaultMongoUrl;
-cfg.mongoUrlTest = process.env.MONGO_URL_TEST || defaultMongoUrlTest;
+  // MongoDB connection string - MONGO_URL is for local dev,
+  mongoUri: process.env.MONGO_URI,
+  mongoUriTest: process.env.MONGO_URI_TEST,
+};
 
-// Export configuration object
-module.exports = cfg;
